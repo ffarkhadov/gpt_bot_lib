d3e24220af8b89cb56ef6bf628f25afb5f6011a1
ffarkhadov <143689743+ffarkhadov@users.noreply.github.com>
2025-06-09 17:37:21 +0400

    Update states.py

--- telegram/states.py
+++ telegram/states.py
@@ -3,7 +3,8 @@ from aiogram.fsm.state import State, StatesGroup
 
 class AddStore(StatesGroup):
     choose_mp = State()
-    client_id = State()      # Ozon only
+    client_id = State()     # только для Ozon
     api_key = State()
     sheet_id = State()
-    confirm_access = State()
+    confirm = State()
+
