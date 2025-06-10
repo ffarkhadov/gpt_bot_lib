c6fd5ad5ba41c528678ea87aeeded245b2333d89
ffarkhadov <143689743+ffarkhadov@users.noreply.github.com>
2025-06-10 04:52:30 +0400

    Update __init__.py

--- telegram/handlers/__init__.py
+++ telegram/handlers/__init__.py
@@ -2,7 +2,9 @@ from aiogram import Router
 
 from .start import router as start_router
 from .add_store import router as add_router
+from .store import router as store_router   # ← меню магазина
 
 router = Router(name="handlers")
 router.include_router(start_router)
 router.include_router(add_router)
+router.include_router(store_router)
